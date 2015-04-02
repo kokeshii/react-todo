99978dd7521a3b24afe21d309f3975553fed838a
FantastiCole <coleamurphy@gmail.com>
2015-04-02 15:54:12 -0400

    Add titleBar and change styling.
    Add colors, change fonts, change formatting of checkmark in relation to task information.

--- index.ios.js
+++ index.ios.js
@@ -47,10 +47,17 @@ var TodoList = React.createClass({
     var task = MOCKED_TASKS_DATA[0];
     return (
       <View style={styles.container}>
-        <Image source={{uri: CHECKMARK_IMG_URL}} style={styles.checkmarkIcon} />
+        <View style={styles.topContainer}>
+          <Text style={styles.titleBar}>To-Do List!</Text>
+        </View>
         <View style={styles.rightContainer}>
-          <Text>{task.title}</Text>
-          <Text>{task.dateDue.toDateString()}</Text>
+          <View style={styles.checkContainer}>
+            <Image source={{uri: CHECKMARK_IMG_URL}} style={styles.checkmarkIcon} />
+          </View>
+          <View style={styles.taskContainer}>
+          <Text style={styles.title}>{task.title}</Text>
+          <Text style={styles.dateDue}>{task.dateDue.toDateString()}</Text>
+          </View>
         </View>
       </View>
     );
@@ -60,10 +67,10 @@ var TodoList = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#C6E2FF',
   },
   welcome: {
     fontSize: 20,
@@ -72,15 +79,73 @@ var styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FF7373',
     marginBottom: 5,
+  },
+  topContainer: {
+    // flex: 1,
+    alignItems: 'center',
+    marginTop: 30,
+    height: 40,
+    width: 375,
+    borderWidth: 1,
+    borderColor: '#003366',
+    backgroundColor: 'rgba(255, 115, 115, 0.2)',
+    margin: 10,
+  },
+  titleBar: {
+    fontFamily: 'Cochin',
+    fontWeight: 'bold',
+    color: '#003366',
+    fontSize: 35,
+  },
+  checkContainer: {
+    borderRightWidth: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    borderColor: '#003366',
+    backgroundColor: 'rgba(250, 250, 250, 0.3)',
   },
   checkmarkIcon: {
     width: 35,
-    height: 35
+    height: 35,
   },
   rightContainer: {
+    // flex: 1,
+    width: 375,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#003366',
+    backgroundColor: 'rgba(250, 250, 250, 0.3)',
+  },
+  taskContainer: {
     flex: 1,
+    // borderWidth: 1,
+    // borderColor: '#003366',
+    // backgroundColor: 'rgba(250, 250, 250, 0.3)',
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Cochin',
+    fontWeight: 'bold',
+    color: '#FF7373',
+    textAlign: 'left',
+    marginBottom: 1,
+    marginLeft: 10,
+  },
+  dateCreated: {
+    fontSize: 15,
+    fontFamily: 'Cochin',
+    color: '#3EBFBB',
+    textAlign: 'left',
+    marginLeft: 10,
+  },
+  dateDue: {
+    fontSize: 15,
+    fontFamily: 'Cochin',
+    color: '#3EBFBB',
+    textAlign: 'left',
+    marginLeft: 10,
   },
 });
 
