e4e501584aa3e38881e0bcb7507bb1e5655d1549
FantastiCole <coleamurphy@gmail.com>
2015-04-07 12:18:31 -0400

    Update object sizing.
    Added more example tasks to list and changed task width to be full without actual width in points.

--- index.ios.js
+++ index.ios.js
@@ -20,6 +20,18 @@ var MOCKED_TASKS_DATA = [
     dateDue: new Date(2016, 1, 1),
   },
 
+  {
+    title: 'Test Task 3',
+    dateCreated: new Date(),
+    dateDue: new Date(2017, 9, 12),
+  },
+
+  {
+    title: 'Test Task 4',
+    dateCreated: new Date(),
+    dateDue: new Date(2018, 11, 30),
+  },
+
 ];
 
 var CHECKMARK_IMG_URL = "http://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png";
@@ -49,7 +61,14 @@ var TodoList = React.createClass({
     return (
       <View style={styles.container}>
         <View style={styles.topContainer}>
-          <Text style={styles.titleBar}>To-Do List!</Text>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleBar}>To-Do List!</Text>
+          </View>
+          <TouchableHighlight onPress={ () => console.log("Add Task") }>
+            <View style={styles.plusContainer}>
+              <Text style={styles.plus}> + </Text>
+            </View>
+          </TouchableHighlight>
         </View>
         <View style={styles.rightContainer}>
           <View style={styles.checkContainer}>
@@ -58,8 +77,41 @@ var TodoList = React.createClass({
             </TouchableHighlight>
           </View>
           <View style={styles.taskContainer}>
-          <Text style={styles.title}>{task.title}</Text>
-          <Text style={styles.dateDue}>{task.dateDue.toDateString()}</Text>
+            <Text style={styles.title}>{task.title}</Text>
+            <Text style={styles.dateDue}>{task.dateDue.toDateString()}</Text>
+          </View>
+        </View>
+        <View style={styles.rightContainer}>
+          <View style={styles.checkContainer}>
+            <TouchableHighlight onPress={ () => console.log("hiiee") }>
+                 <Image source={{uri: CHECKMARK_IMG_URL}} style={styles.checkmarkIcon} />
+            </TouchableHighlight>
+          </View>
+          <View style={styles.taskContainer}>
+            <Text style={styles.title}>{MOCKED_TASKS_DATA[1].title}</Text>
+            <Text style={styles.dateDue}>{MOCKED_TASKS_DATA[1].dateDue.toDateString()}</Text>
+          </View>
+        </View>
+        <View style={styles.rightContainer}>
+          <View style={styles.checkContainer}>
+            <TouchableHighlight onPress={ () => console.log("hiiee") }>
+                 <Image source={{uri: CHECKMARK_IMG_URL}} style={styles.checkmarkIcon} />
+            </TouchableHighlight>
+          </View>
+          <View style={styles.taskContainer}>
+            <Text style={styles.title}>{MOCKED_TASKS_DATA[2].title}</Text>
+            <Text style={styles.dateDue}>{MOCKED_TASKS_DATA[2].dateDue.toDateString()}</Text>
+          </View>
+        </View>
+        <View style={styles.rightContainer}>
+          <View style={styles.checkContainer}>
+            <TouchableHighlight onPress={ () => console.log("hiiee") }>
+                 <Image source={{uri: CHECKMARK_IMG_URL}} style={styles.checkmarkIcon} />
+            </TouchableHighlight>
+          </View>
+          <View style={styles.taskContainer}>
+            <Text style={styles.title}>{MOCKED_TASKS_DATA[3].title}</Text>
+            <Text style={styles.dateDue}>{MOCKED_TASKS_DATA[3].dateDue.toDateString()}</Text>
           </View>
         </View>
       </View>
@@ -74,8 +126,8 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
     backgroundColor: '#C6E2FF',
   },
   welcome: {
@@ -89,15 +141,36 @@ var styles = StyleSheet.create({
     marginBottom: 5,
   },
   topContainer: {
-    // flex: 1,
-    alignItems: 'center',
+    flexDirection: 'row',
+    // alignSelf: 'flex-start',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    top: 0,
     marginTop: 30,
     height: 40,
-    width: 375,
-    borderWidth: 1,
-    borderColor: '#003366',
-    backgroundColor: 'rgba(255, 115, 115, 0.2)',
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    borderRadius: 5,
+    backgroundColor: 'rgba(250, 250, 250, 0.2)',
     margin: 10,
+  },
+  plusContainer: {
+    flex: 1,
+    borderLeftWidth: 1,
+    justifyContent: 'flex-end',
+    borderColor: '#FFFFFF',
+    backgroundColor: 'rgba(250, 250, 250, 0.3)',
+    alignItems: 'center',
+  },
+  plus: {
+    fontFamily: 'Cochin',
+    fontWeight: 'bold',
+    color: '#003366',
+    fontSize: 35,
+  },
+  titleContainer: {
+    flex: 7,
+    alignItems: 'center',
   },
   titleBar: {
     fontFamily: 'Cochin',
@@ -106,29 +179,25 @@ var styles = StyleSheet.create({
     fontSize: 35,
   },
   checkContainer: {
+    // flex: 1,
     borderRightWidth: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    borderColor: '#003366',
+    borderColor: '#FFFFFF',
     backgroundColor: 'rgba(250, 250, 250, 0.3)',
   },
   checkmarkIcon: {
+    margin: 4,
     width: 35,
     height: 35,
   },
   rightContainer: {
-    // flex: 1,
-    width: 375,
     flexDirection: 'row',
     borderWidth: 1,
-    borderColor: '#003366',
+    borderRadius: 5,
+    borderColor: '#FFFFFF',
     backgroundColor: 'rgba(250, 250, 250, 0.3)',
   },
   taskContainer: {
     flex: 1,
-    // borderWidth: 1,
-    // borderColor: '#003366',
-    // backgroundColor: 'rgba(250, 250, 250, 0.3)',
   },
   title: {
     fontSize: 20,
